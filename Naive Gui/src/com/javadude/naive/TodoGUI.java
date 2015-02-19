dc95d9d04e752e50fece0724d6f5c9abdbc908b7
Scott Stanchfield <scott@javadude.com>
2015-02-19 00:29:06 -0500

    Changed to procedural creation

--- Naive Gui/src/com/javadude/naive/TodoGUI.java
+++ Naive Gui/src/com/javadude/naive/TodoGUI.java
@@ -12,34 +12,86 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TodoGUI {
-	// Ewwwwwwwwwwwwwwwwwwwwww...............
+	// A little better (very procedural)
 	public static void main(String[] args) {
-		List list = new List();
-		TextField itemField = new TextField();
-		Button addButton = new Button("Add");
-		Button removeButton = new Button("Remove");
-		
-		addButton.addActionListener(e -> list.add(itemField.getText()));
-		removeButton.addActionListener(e -> list.remove(list.getSelectedIndex()));
+		new TodoGUI().go();
+	}
 
-		Panel buttons = new Panel(new GridLayout(1,0,3,3));
-		buttons.add(addButton);
-		buttons.add(removeButton);
-		Panel bottom = new Panel(new FlowLayout(FlowLayout.RIGHT));
-		bottom.add(buttons);
-		Panel centerPanel = new Panel(new BorderLayout());
-		centerPanel.add(BorderLayout.NORTH, itemField);
-		centerPanel.add(BorderLayout.SOUTH, buttons);
+	private Button addButton;
+	private Button removeButton;
+	private List list;
+	private TextField itemField;
+	private Frame frame;
+	private Panel centerPanel;
+	private Panel bottomPanel;
+	private Panel buttonsPanel;
 
-		Frame frame = new Frame();
-		frame.setLayout(new BorderLayout());
-		frame.add(BorderLayout.WEST, list);
-		frame.add(BorderLayout.CENTER, centerPanel);
-		frame.pack();
-		frame.addWindowListener(new WindowAdapter() {
-			@Override public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}});
-		frame.setVisible(true);
+	private void go() {
+		createFrame().setVisible(true);
+	}
+
+	private Frame createFrame() {
+		if (frame == null) {
+			frame = new Frame();
+			frame.setLayout(new BorderLayout());
+			frame.add(BorderLayout.WEST, getList());
+			frame.add(BorderLayout.CENTER, createCenterPanel());
+			frame.addWindowListener(new WindowAdapter() {
+				@Override public void windowClosing(WindowEvent e) {
+					System.exit(0);
+				}});
+			frame.pack();
+		}
+		return frame;
+	}
+
+	private List getList() {
+		if (list == null) {
+			list = new List();
+		}
+		return list;
+	}
+	private TextField getItemField() {
+		if (itemField == null) {
+			itemField = new TextField();
+		}
+		return itemField;
+	}
+	private Panel createCenterPanel() {
+		if (centerPanel == null) {
+			centerPanel = new Panel(new BorderLayout());
+			centerPanel.add(BorderLayout.NORTH, getItemField());
+			centerPanel.add(BorderLayout.SOUTH, createBottom());
+		}
+		return centerPanel;
+	}
+	private Panel createButtons() {
+		if (buttonsPanel == null) {
+			buttonsPanel = new Panel(new GridLayout(1,0,3,3));
+			buttonsPanel.add(createAddButton());
+			buttonsPanel.add(createRemoveButton());
+		}
+		return buttonsPanel;
+	}
+	private Panel createBottom() {
+		if (bottomPanel == null) {
+			bottomPanel = new Panel(new FlowLayout(FlowLayout.RIGHT));
+			bottomPanel.add(createButtons());
+		}
+		return bottomPanel;
+	}
+	private Button createAddButton() {
+		if (addButton == null) {
+			addButton = new Button("Add");
+			addButton.addActionListener(e -> getList().add(getItemField().getText()));
+		}
+		return addButton;
+	}
+	private Button createRemoveButton() {
+		if (removeButton == null) {
+			removeButton = new Button("Remove");
+			removeButton.addActionListener(e -> getList().remove(getList().getSelectedIndex()));
+		}
+		return removeButton;
 	}
 }
