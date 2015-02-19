4c19ab12c7f4f89692142da1a3a0ec3c877b52ea
Scott Stanchfield <scott@javadude.com>
2015-02-19 00:34:26 -0500

    Compact OO

--- Naive Gui/src/com/javadude/naive/TodoGUI.java
+++ Naive Gui/src/com/javadude/naive/TodoGUI.java
@@ -12,86 +12,38 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TodoGUI {
-	// A little better (very procedural)
+	// Compact inline objects (OO, but possibly wonky to read)
 	public static void main(String[] args) {
 		new TodoGUI().go();
 	}
 
-	private Button addButton;
-	private Button removeButton;
 	private List list;
 	private TextField itemField;
-	private Frame frame;
-	private Panel centerPanel;
-	private Panel bottomPanel;
-	private Panel buttonsPanel;
 
+	@SuppressWarnings("serial")
 	private void go() {
-		createFrame().setVisible(true);
-	}
-
-	private Frame createFrame() {
-		if (frame == null) {
-			frame = new Frame();
-			frame.setLayout(new BorderLayout());
-			frame.add(BorderLayout.WEST, getList());
-			frame.add(BorderLayout.CENTER, createCenterPanel());
-			frame.addWindowListener(new WindowAdapter() {
+		new Frame() {{
+			setLayout(new BorderLayout());
+			add(BorderLayout.WEST, list=new List());
+			add(BorderLayout.CENTER, new Panel(new BorderLayout()) {{
+				add(BorderLayout.NORTH, itemField=new TextField());
+				add(BorderLayout.SOUTH, new Panel(new FlowLayout(FlowLayout.RIGHT)) {{
+					add(new Panel(new GridLayout(1,0,3,3)) {{
+						add(new Button("Add") {{
+							addActionListener(e -> list.add(itemField.getText()));
+						}});
+						add(new Button("Remove") {{
+							addActionListener(e -> list.remove(list.getSelectedIndex()));
+						}});
+					}});
+				}});
+			}});
+			addWindowListener(new WindowAdapter() {
 				@Override public void windowClosing(WindowEvent e) {
 					System.exit(0);
 				}});
-			frame.pack();
-		}
-		return frame;
-	}
-
-	private List getList() {
-		if (list == null) {
-			list = new List();
-		}
-		return list;
-	}
-	private TextField getItemField() {
-		if (itemField == null) {
-			itemField = new TextField();
-		}
-		return itemField;
-	}
-	private Panel createCenterPanel() {
-		if (centerPanel == null) {
-			centerPanel = new Panel(new BorderLayout());
-			centerPanel.add(BorderLayout.NORTH, getItemField());
-			centerPanel.add(BorderLayout.SOUTH, createBottom());
-		}
-		return centerPanel;
-	}
-	private Panel createButtons() {
-		if (buttonsPanel == null) {
-			buttonsPanel = new Panel(new GridLayout(1,0,3,3));
-			buttonsPanel.add(createAddButton());
-			buttonsPanel.add(createRemoveButton());
-		}
-		return buttonsPanel;
-	}
-	private Panel createBottom() {
-		if (bottomPanel == null) {
-			bottomPanel = new Panel(new FlowLayout(FlowLayout.RIGHT));
-			bottomPanel.add(createButtons());
-		}
-		return bottomPanel;
-	}
-	private Button createAddButton() {
-		if (addButton == null) {
-			addButton = new Button("Add");
-			addButton.addActionListener(e -> getList().add(getItemField().getText()));
-		}
-		return addButton;
-	}
-	private Button createRemoveButton() {
-		if (removeButton == null) {
-			removeButton = new Button("Remove");
-			removeButton.addActionListener(e -> getList().remove(getList().getSelectedIndex()));
-		}
-		return removeButton;
+			pack();
+			setVisible(true);
+		}};
 	}
 }
